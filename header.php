f2cea1877ff00d7ac542715fd083370a9943f7f2
CrunchXnoy <baby123kucing@gmail.com>
2024-12-18 21:39:00 +0700

    update filter, update file

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
         <a id="logo" href="#home"><img src="https://github.com/carlosavilae/Netflix-Clone/blob/master/img/logo.PNG?raw=true" alt="Logo Image"></a>
     </div>
     <nav class="main-nav">
-        <a href="#home">Home</a>
+        <a href="/project_api">Home</a>
         <a href="category.php">Category</a>
         <a href="filter.php">Filter</a>
     </nav>
